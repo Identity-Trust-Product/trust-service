ebb6ca46605808ef47e672ae9f6c712a55240190
dajinkya1099 <dajinkya1099@gmail.com>
2026-08-14 17:23:05 +0530

    Initial commit: trust service

---
+++ src/main/java/com/identityos/trust_service/TrustServiceApplication.java
@@ -0,0 +1,13 @@
+package com.identityos.trust_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/identityos/trust_service/TrustServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identityos.trust_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
